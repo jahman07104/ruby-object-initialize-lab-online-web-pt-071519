1e349c9ff54f39b9a1db9d856801b73f88bb0425
Patrick Harrison <jahman@bellatlantic.net>
2019-08-07 23:41:07 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,13 @@
 class Person
- def initialize(name)
-   @name=name
+  def initialize(name)
+    @name=name
  end
  def name=(name)
-   @name=name
+    @name=name
  end
  def name
    @name
- 
+ end
 end
 
 beyonce = Person.new("Beyonce")
