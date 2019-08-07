949bf8d2888d4ba37f6963e659fcb2b89cbbd408
Patrick Harrison <jahman@bellatlantic.net>
2019-08-07 23:56:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,17 +12,17 @@ end
 
 fido = Dog.new("Fido")
 fido.name
-  def initialize(breed)
-    @breed=breed
- end
- def breed=(breed)
-    @breed=breed
- end
- def breed
-   @breed
- end
+#   def initialize(breed)
+#     @breed=breed
+# end
+# def breed=(breed)
+#     @breed=breed
+# end
+# def breed
+#   @breed
+# end
 
 
-fido = Dog.new("Fido",)
-fido.name
-fido.breed
+# fido = Dog.new("Fido",)
+# fido.name
+# fido.breed
